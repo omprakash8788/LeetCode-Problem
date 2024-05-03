196949eb76c18be5714911ef74de5ae72f58ffd5
omprakash8788 <omprakashkumar552@gmail.com>
2024-05-03 08:08:45 +0530

    dsa leetcode problem 3

---
+++ problem2.js
@@ -0,0 +1,44 @@
+// 3. Longest Substring Without Repeating Characters
+// Solved
+// Medium
+// Topics
+// Companies
+// Hint
+// Given a string s, find the length of the longest 
+// substring
+//  without repeating characters.
+// Example 1:
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+// Constraints:
+
+// 0 <= s.length <= 5 * 104
+// s consists of English letters, digits, symbols and spaces.
+
+let s="abcabcbb"
+let maxLength=0;
+let start=0;
+let charMap={}
+for(let end=0; end<s.length; end++){
+    const currentChar=s[end];
+    if(charMap[currentChar]!==undefined){
+        start=Math.max(start, charMap[currentChar]+1);
+
+    }
+    charMap[currentChar]=end;
+    maxLength=Math.max(maxLength, end-start+1);
+}
+console.log(maxLength)
+
